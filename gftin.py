cb8856a8448767ca2c1cab828c200845ffedb0c5
xdzhaooo <150081265+xdzhaooo@users.noreply.github.com>
2024-01-04 16:04:17 +0100

    success

--- gftin.py
+++ gftin.py
@@ -7,8 +7,8 @@ import startinpy
 def cropped_laz(filename='./46AN1_20.LAZ'):
     las = laspy.read(filename)
 
-    x_min, x_max = 184130, 184630
-    y_min, y_max = 420410, 420900
+    x_min, x_max = 184130, 184180
+    y_min, y_max = 420410, 420460
 
     mask = (
             (las.x >= x_min) & (las.x <= x_max) &
@@ -23,13 +23,13 @@ def cropped_laz(filename='./46AN1_20.LAZ'):
 
     return cropped_las
 
-def gftin(cropped_las, r_max=2.0, alpha_max=0.2):
+def gftin(cropped_las, r_max=1.0, alpha_max=0.5):
     # step 1, extract the lowest points from grids
     min_x, max_x = min(cropped_las[:, 0]), max(cropped_las[:, 0])
     min_y, max_y = min(cropped_las[:, 1]), max(cropped_las[:, 1])
     # print(min_x,min_y,max_x,max_y)
 
-    grid_len = 80
+    grid_len = 20
     i, j = min_x, min_y
     lowest_p = []
     while i <= max_x and j <= max_y:
@@ -55,34 +55,41 @@ def gftin(cropped_las, r_max=2.0, alpha_max=0.2):
     # r_dl.calculate_tri()
     # tin = r_dl.triangles
 
-
-    print(len(tin))
+    dt=startinpy.DT()
+    dt.insert(lowest_p)
+    new_dl = dt
 
     # step2&3 Tin refinement,First traverse the points, calculate epslon, and insert from the point closest to tin
     # until there are no points within epslon.
     e_r = 0
     e_alpha = 0
-    new_dl = r_dl.copy()
     allpts = cropped_las.copy()
+    print('len',len(allpts))
     while e_r < r_max and e_alpha < alpha_max and len(allpts)>0:
         e_r = r_max
         e_alpha = alpha_max
-        for i in range(len(allpts)):
-            pt = allpts[i]
-            for i in range(len(new_dl.triangles)):
-                tri = list(new_dl.triangles[i])
-                print(1)
+        insert_pt = None
+        for m in range(len(allpts)):
+            pt = allpts[m]
+            for n in range(len(new_dl.triangles)):
+                tri_list = list(new_dl.triangles[n])
+                tri = new_dl.points[tri_list]
                 if p_within_tri(pt,tri):
                     r, alpha = cal_r_alpha(pt,tri)
-                    print(2)
-                if e_r > r and e_alpha > alpha:              # 写到一行对吗，不确定？？？？？？？？？？？
-                    e_r = r
-                    e_alpha = alpha
-                    i_selected = i
-        new_dl.points.append(allpts[i_selected])
-        new_dl.calculate_tri()
-        del allpts[i_selected]
+                    if e_r > r and e_alpha > alpha:              # 写到一行对吗，不确定？？？？？？？？？？？
+                        e_r = r
+                        e_alpha = alpha
+                        i_selected = m
+                        insert_pt = allpts[i_selected][0],allpts[i_selected][1],allpts[i_selected][2]
+        print(insert_pt)
+        if insert_pt:
+            new_dl.insert_one_pt(allpts[i_selected][0], allpts[i_selected][1],allpts[i_selected][2])
+            allpts = np.delete(allpts,i_selected,axis=0)
+            print(len(allpts))
 
+        print(len(new_dl.points))
+
+    return new_dl.points
 
 def p_within_tri(pt,tri):
     ax = tri[0][0]
@@ -91,9 +98,8 @@ def p_within_tri(pt,tri):
     by = tri[1][1]
     cx = tri[2][0]
     cy = tri[2][1]
-    ptx=pt[0]
-    pty=pt[1]
-
+    ptx = pt[0]
+    pty = pt[1]
     # calculate tri_area
     ABC = 0.5 * abs(ax * (by - cy) + bx * (cy - ay) + cx * (ay - by))
 
@@ -127,11 +133,13 @@ def cal_r_alpha(pt,tri):
     if any(d == 0 for d in distances_to_vertices):
         distance = 0.0
 
-    beta_1 = np.arcsin(distance/l1)
-    beta_2 = np.arcsin(distance/l2)
-    beta_3 = np.arcsin(distance/l3)
-    alpha = max(beta_3,beta_2,beta_1)
-    print(alpha)
+    if l1!=0 and l2!=0 and l3!=0:
+        beta_1 = np.arcsin(distance/l1)
+        beta_2 = np.arcsin(distance/l2)
+        beta_3 = np.arcsin(distance/l3)
+        alpha = max(beta_3,beta_2,beta_1)
+    else:
+        alpha = np.inf
 
     return distance,alpha
 
